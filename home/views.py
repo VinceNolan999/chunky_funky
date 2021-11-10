84dfc21286c829855f7ff59b3e56abe3506ee8e9
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-11-10 18:52:06 +0000

    home app / templates added.

---
+++ home/views.py
@@ -0,0 +1,13 @@
+"""
+views for the home app
+"""
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    """ a view to return the index page """
+
+    return render(request, 'home/index.html')
