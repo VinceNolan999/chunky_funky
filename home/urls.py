bc31ac9771a36a21ca65e9a10e987d46067b4270
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-11-16 22:40:18 +0000

    product page created

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,9 @@
 """
 Home app URL Configuration
 """
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='home')
 ]
