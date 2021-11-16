dedacda13d30a885bdc026e7adaad2bb9070a150
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-11-16 10:38:39 +0000

    django admin display customized

--- products/models.py
+++ products/models.py
@@ -7,9 +7,10 @@ from django.db import models
 
 
 class Category(models.Model):
-    """
-    category fields
-    """
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
