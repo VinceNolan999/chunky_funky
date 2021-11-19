5d663c25434a6ec98378be3d95d7c2c4baa2ed0b
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-11-19 19:42:46 +0000

    added toasts

--- products/views.py
+++ products/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
-
 # Create your views here.
 
 
