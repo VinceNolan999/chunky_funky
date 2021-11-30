e9e11631f28cbf71a8396fe3c1fcb1a082f5165c
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-11-30 21:40:24 +0000

    added profile/forms/models/views and template/css

---
+++ profiles/forms.py
@@ -0,0 +1,40 @@
+'''
+creates the form for the profile page
+'''
+
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    ''' user profile form '''
+    class Meta:
+        ''' sets user details from the models '''
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_town_or_city': 'Town or City',
+            'default_county': 'County, State or Locality',
+            'default_postcode': 'Postal Code',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
+            self.fields[field].label = False
