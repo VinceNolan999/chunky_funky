1b08c3615dedbb45b168c4d83dccf592bd34a576
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-12-01 19:06:26 +0000

    added add product views and links

--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+from .forms import ProductForm
 from .models import Product, Category
 
 # Create your views here.
@@ -72,3 +73,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
