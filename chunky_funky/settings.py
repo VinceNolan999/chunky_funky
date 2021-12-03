b41d3cc19d0ce92253ff095ca263161e97cab3ba
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-12-03 20:38:40 +0000

    secret key and debug to environ

--- chunky_funky/settings.py
+++ chunky_funky/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+04$o$!nfld6i-m@*ty&h5&5&=+t_fapjdg_m4-nx_vuhi2)oi'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['chunky-funky.herokuapp.com', 'localhost']
 
