50954480a141c9105e1b6c181638eb7d619a03f5
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-12-03 13:27:10 +0000

    product image fixes

--- products/forms.py
+++ products/forms.py
@@ -4,6 +4,7 @@ Creates form for the product admin sections
 
 from django import forms
 from .models import Product, Category
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
@@ -11,6 +12,7 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,4 +22,3 @@ class ProductForm(forms.ModelForm):
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
-
